b13daecb81606ba45c0a9406b594db26147cb4cb
majkel93 <michal20024@wp.pl>
2017-08-15 22:14:10 +0200

    Utworzenie funkcji odejmowania

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+include <stdio.h>
 
 
 int dodaj(int x, int y) {
 	return x+y;
 }
 
+int odejmowanie(int x, int y) {
+	return x-y;
+}
+
 int main() {
-	
-	
-	
 	printf("Hello wordl");
 	printf('%d',dodaj(2,3));
+	printf("%d".odejmowanie(5,6));
 	return 0;
 }
